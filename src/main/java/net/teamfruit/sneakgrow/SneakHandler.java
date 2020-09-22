90d5b584d1a247e6f416037fd9844552f6a8cc56
Kamesuta <kamesuta@gmail.com>
2020-09-23 05:20:27 +0900

    mob grow

--- src/main/java/net/teamfruit/sneakgrow/SneakHandler.java
+++ src/main/java/net/teamfruit/sneakgrow/SneakHandler.java
@@ -2,9 +2,9 @@ package net.teamfruit.sneakgrow;
 
 import org.bukkit.*;
 import org.bukkit.block.Block;
-import org.bukkit.block.data.Ageable;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.block.data.type.Sapling;
+import org.bukkit.entity.Ageable;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,13 +12,15 @@ import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 public class SneakHandler implements Listener {
     private Map<String, PlayerState> states = new HashMap<>();
 
     private static final ItemStack boneMeal = new ItemStack(Material.BONE_MEAL);
     private static final Object nmsBoneMeal = ReflectionUtil.itemStackAsNmsCopy(boneMeal);
+
+    private final Random rnd = new Random();
 
     public class PlayerState {
         public boolean isSneaking;
@@ -32,60 +34,85 @@ public class SneakHandler implements Listener {
     }
 
     private void onAction(Player player) {
-        if (!player.hasPermission("sneakgrow"))
+        boolean bPermissionBlock = player.hasPermission("sneakgrow.block");
+        boolean bPermissionEntity = player.hasPermission("sneakgrow.entity");
+        if (!bPermissionBlock && !bPermissionEntity)
             return;
 
         PlayerState state = states.computeIfAbsent(player.getName(), e -> new PlayerState());
 
-        {
-            int ticksNow = Bukkit.getCurrentTick();
-            int ticksSinceLastCheck = ticksNow - state.ticksLastCheck;
-            if (ticksSinceLastCheck >= SneakGrow.cooldown) {
-                state.ticksLastCheck = ticksNow;
+        int ticksNow = Bukkit.getCurrentTick();
+        int ticksSinceLastCheck = ticksNow - state.ticksLastCheck;
+        if (ticksSinceLastCheck >= SneakGrow.cooldown) {
+            state.ticksLastCheck = ticksNow;
 
-                World world = player.getWorld();
-                Object nmsWorld = ReflectionUtil.craftWorldGetHandle(world);
-                Location location = player.getLocation();
-                List<Block> coords = getNearestBlocks(world, location.getBlockX(), location.getBlockY(), location.getBlockZ());
+            World world = player.getWorld();
+            Object nmsWorld = ReflectionUtil.craftWorldGetHandle(world);
+            Location location = player.getLocation();
 
-                if (state.isSneaked) {
-                    if (coords.size() > 0) {
+            if (state.isSneaked) {
+                if (bPermissionBlock) {
+                    Optional<Block> blockOptional = getRandomElement(getAgeableBlockInRange(location));
 
-                        Collections.shuffle(coords);
-
-                        for (Block block : coords) {
-                            if (ThreadLocalRandom.current().nextFloat() < .45) {
-                                Object nmsBlockPosition = ReflectionUtil.constructBlockPosition(block.getX(), block.getY(), block.getZ());
-                                ReflectionUtil.applyBoneMeal(nmsBoneMeal, nmsWorld, nmsBlockPosition);
-                            }
-
-                            if (SneakGrow.showParticles)
-                                sendPacket(block);
-
-                            break;
+                    blockOptional.ifPresent(block -> {
+                        if (rnd.nextFloat() < .45) {
+                            Object nmsBlockPosition = ReflectionUtil.constructBlockPosition(block.getX(), block.getY(), block.getZ());
+                            ReflectionUtil.applyBoneMeal(nmsBoneMeal, nmsWorld, nmsBlockPosition);
                         }
-                    }
 
-                    state.isSneaked = false;
+                        if (SneakGrow.showParticles)
+                            sendPacketGrowBlock(block.getLocation());
+                    });
                 }
-            }
 
-            boolean isSneaking = player.isSneaking();
-            if (state.isSneaking != isSneaking) {
-                state.isSneaking = isSneaking;
-                state.isSneaked = true;
+                if (bPermissionEntity) {
+                    Optional<Ageable> entityOptional = getRandomElement(getAgeableEntityInRange(location));
+
+                    entityOptional.ifPresent(entity -> {
+                        if (rnd.nextFloat() < .25) {
+                            entity.setAge(entity.getAge() + 1);
+                            entity.setBreed(true);
+                        }
+
+                        if (SneakGrow.showParticles)
+                            sendPacketGrowEntity(entity.getLocation());
+                    });
+                }
+
+                state.isSneaked = false;
             }
+        }
+
+        boolean isSneaking = player.isSneaking();
+        if (state.isSneaking != isSneaking) {
+            state.isSneaking = isSneaking;
+            state.isSneaked = true;
         }
     }
 
-    private void sendPacket(Block block) {
-        Location location = block.getLocation();
+    private <T> Optional<T> getRandomElement(List<T> elements) {
+        if (elements.isEmpty())
+            return Optional.empty();
+        return Optional.ofNullable(elements.get(rnd.nextInt(elements.size() - 1)));
+    }
+
+    private void sendPacketGrowBlock(Location location) {
         Collection<Player> players = location.getNearbyPlayers(48);
         for (Player player : players)
             player.playEffect(location, Effect.VILLAGER_PLANT_GROW, Integer.valueOf(0));
     }
 
-    private List<Block> getNearestBlocks(World world, int centerX, int centerY, int centerZ) {
+    private void sendPacketGrowEntity(Location location) {
+        Collection<Player> players = location.getNearbyPlayers(48);
+        for (Player player : players)
+            player.spawnParticle(Particle.VILLAGER_HAPPY, location, 5, .2, .2, .2);
+    }
+
+    private List<Block> getAgeableBlockInRange(Location location) {
+        World world = location.getWorld();
+        int centerX = location.getBlockX();
+        int centerY = location.getBlockY();
+        int centerZ = location.getBlockZ();
         List<Block> list = new ArrayList<>();
         for (int x = -5; x <= 5; x++)
             for (int y = -2; y <= 2; y++)
@@ -94,12 +121,20 @@ public class SneakHandler implements Listener {
                     BlockData blockData = block.getBlockData();
                     if (SneakGrow.enableSaplings && blockData instanceof Sapling) {
                         list.add(block);
-                    } else if (SneakGrow.enableCrops && blockData instanceof Ageable) {
-                        Ageable data = (Ageable) blockData;
+                    } else if (SneakGrow.enableCrops && blockData instanceof org.bukkit.block.data.Ageable) {
+                        org.bukkit.block.data.Ageable data = (org.bukkit.block.data.Ageable) blockData;
                         if (data.getAge() < data.getMaximumAge())
                             list.add(block);
                     }
                 }
         return list;
+    }
+
+    private List<org.bukkit.entity.Ageable> getAgeableEntityInRange(Location location) {
+        return location.getNearbyLivingEntities(5).stream()
+                .filter(e -> e instanceof org.bukkit.entity.Ageable)
+                .map(org.bukkit.entity.Ageable.class::cast)
+                .filter(e -> !e.isAdult() || !e.canBreed())
+                .collect(Collectors.toList());
     }
 }
